f5fd1bf9eca5bea28fa2ae8f1adaca5816132188
Mengxi Wu <mengxiwu@Mengxis-Laptop.local>
2024-10-21 14:26:23 -0700

    test

--- call_class.py
+++ call_class.py
@@ -1,31 +1,35 @@
 import numpy as np
 from classy import Class
 
-for file_name in ['dataset/neff_train_param.npz', 'dataset/neff_val_param.npz', 'dataset/neff_test_param.npz']:
-    data = np.load(file_name)
-    tt = []
-    ll = []
+with open('log.txt', 'w') as f:
 
-    for i in range(len(data["omega_b"])):
+    for file_name in ['dataset/neff_train_param.npz', 'dataset/neff_val_param.npz', 'dataset/neff_test_param.npz']:
+        data = np.load(file_name)
+        tt = []
+        ll = []
 
-        common_settings = {'h':data['h'][i],
-                            'omega_b':data['omega_b'][i],
-                            'omega_cdm':data['omega_cdm'][i],
-                            'ln_A_s_1e10':data['ln_A_s_1e10'][i],
-                            'n_s':data['n_s'][i],
-                            'tau_reio':data['tau_reio'][i],
-                            'N_ur':data['N_eff'][i],
-                            'output':'tCl'}
+        for i in range(len(data["omega_b"])):
 
-        cosmo = Class()
-        cosmo.set(common_settings)
-        cosmo.compute()
-        cl = cosmo.raw_cl(2500)
-        tt.append(cl['tt'][2:])
-        ll.append(cl['ell'][2:])
-        cosmo.struct_cleanup()
-        cosmo.empty()
+            common_settings = {'h':data['h'][i],
+                                'omega_b':data['omega_b'][i],
+                                'omega_cdm':data['omega_cdm'][i],
+                                'ln_A_s_1e10':data['ln_A_s_1e10'][i],
+                                'n_s':data['n_s'][i],
+                                'tau_reio':data['tau_reio'][i],
+                                'N_ur':data['N_eff'][i],
+                                'output':'tCl'}
+            
+            f.write(str(i+1))
+            cosmo = Class()
+            cosmo.set(common_settings)
+            cosmo.compute()
+            cl = cosmo.raw_cl(2500)
+            tt.append(cl['tt'][2:])
+            ll.append(cl['ell'][2:])
+            cosmo.struct_cleanup()
+            cosmo.empty()
 
-    tt = np.array(tt)
-    ll = np.array(ll)
-    np.savez(file_name.replace('param', 'tt'), features=np.log(tt), modes=ll)
+        tt = np.array(tt)
+        ll = np.array(ll)
+        np.savez(file_name.replace('param', 'tt'), features=np.log(tt), modes=ll)
+f.close()
