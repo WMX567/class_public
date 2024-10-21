2fa84663f30b934cc79fa29d3d1f7adefb6427e5
Mengxi Wu <mengxiwu@Mengxis-Laptop.local>
2024-10-21 15:05:53 -0700

    test

--- call_class.py
+++ call_class.py
@@ -1,15 +1,22 @@
 import numpy as np
 import log
 from classy import Class  # 确保你已经导入Class库
+import argparse
+
+
+parser = argparse.ArgumentParser(description='Data Generation')
+parser.add_argument('--start', type=int, default=0, help='start index')
+parser.add_argument('--end', type=int, default=100, help='end index')
+args = parser.parse_args()
 
 io = log.IOStream()
 
-for file_name in ['dataset/neff_train_param.npz', 'dataset/neff_val_param.npz', 'dataset/neff_test_param.npz']:
+for file_name in ['dataset/neff_train_param.npz']: # 'dataset/neff_val_param.npz', 'dataset/neff_test_param.npz']:
     data = np.load(file_name)
     tt = []
     ll = []
 
-    for i in range(len(data["omega_b"])):
+    for i in range(len(data["omega_b"][args.start:args.end])):
       
         common_settings = {
             'h': data['h'][i],
@@ -34,8 +41,8 @@ for file_name in ['dataset/neff_train_param.npz', 'dataset/neff_val_param.npz', 
         cosmo.struct_cleanup()
         cosmo.empty()
 
-    # 将结果保存为新的文件
+
     tt = np.array(tt)
     ll = np.array(ll)
-    output_file = file_name.replace('param', 'tt')
+    output_file = file_name.replace('param', 'tt_{}'.format(args.start))
     np.savez(output_file, features=np.log(tt), modes=ll)
